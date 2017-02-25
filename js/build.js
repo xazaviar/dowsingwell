a1546b5223ac8f4344ab068eee871a07a353923e
Joseph Ryan <xazaviar@gmail.com>
2017-02-25 17:36:47 -0500

    Updating build.js

--- js/build.js
+++ js/build.js
@@ -1,13 +1,38 @@
-buildpage(1);
+$(document).ready(function() {
+	buildHome();
+});
 
 
 function buildpage(id){
+	$(".main").empty();
 
-	if(id==0){
+	if(id === 0){
 		buildHome();
+	}else if (id === 1){
+		buildWebsiteView();
 	}
 }
 
 function buildHome(){
+	$(".main").append('<div class="center"><div class="logo"></div></div>');
+	$(".main .center").append('<form><input type="text" name="search" placeholder="Search"><div class="buttons"><input type="radio" name="Searching" value="exploring" checked>Exploring<input type="radio" name="Searching" value="indexed">Indexed</div></form>');
+
+	//Change The background color
+	var colors = [[255,0,0,0.45],
+				  [255,0,255,0.45],
+				  [0,255,0,0.45],
+				  [0,0,255,0.45],
+				  [66,244,238,0.45],
+				  [244,232,65,0.65],
+				  [127,127,127,0.45],
+				  [255,89,249,0.45],
+				  [160,80,0,0.45]];
+	var r = (parseInt(Math.random()*100))%colors.length;
+
+	$(".main").css('background','linear-gradient(rgba('+colors[r][0]+','+colors[r][1]+','+colors[r][2]+',' +colors[r][3]+'),rgba('+colors[r][0]+','+colors[r][1]+','+colors[r][2]+',' +colors[r][3]+')),url("./images/background3.png") no-repeat center center fixed');
+
+}
+
+function buildWebsiteView(url){
 	
 }
